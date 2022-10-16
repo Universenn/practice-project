bb3d6da45f54be44b34afd22e9eea13034106194
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-16 17:15:07 +0900

    게시판 만들기 -우주완-

---
+++ juwan/console/src/main/java/seeboard/BoardPractice.java
@@ -0,0 +1,8 @@
+package lionlike.team13.juwan.console.src.main.java.seeboard;
+
+public class BoardPractice {
+    public static void main(String[] args) {
+        MainMenu fm = new MainMenu();
+        fm.printmenu();
+    }
+}
